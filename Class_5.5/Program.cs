7f7a58617eddf9696ce5bca3a0a1e47710a5f5bf
Irina <spartak.irina92@gmail.com>
2021-11-28 18:15:16 +0700

    Добавьте файлы проекта.

---
+++ Class_5.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество n строк и столбцов массива ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = 1 - (i + j) % 2;
+                    Console.Write("{0,2} ", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
